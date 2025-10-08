b6c600c7f3733664d7fdbe0d7433388911a02a16
Vincent Pellé <vincent.pelle@pm.me>
2025-10-08 11:15:52 +0200

    seed an body types

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+## seed Assemblee Nationale body types
+body_type_list = YAML.load_file(Rails.root.join("db","seeds","an_body_types.yml"))
+
+An::BodyType.upsert_all(body_type_list, unique_by: :code)
