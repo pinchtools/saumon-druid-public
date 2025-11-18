f98a17b079c2064d7f98ba2ad4102d3849ccbb91
Vincent Pellé <vincent.pelle@pm.me>
2025-11-18 12:33:11 +0100

    order agent_version#models by priority by defaul

--- app/models/agent_version.rb
+++ app/models/agent_version.rb
@@ -1,9 +1,9 @@
 class AgentVersion < ApplicationRecord
   belongs_to :agent
-  has_many :agent_version_llm_models, dependent: :destroy
+  has_many :agent_version_llm_models, -> { order(:priority) }, dependent: :destroy
   has_many :llm_models, through: :agent_version_llm_models
 
-  has_many :enabled_agent_version_llm_models, -> { enabled }, class_name: "AgentVersionLlmModel"
+  has_many :enabled_agent_version_llm_models, -> { enabled.order(:priority) }, class_name: "AgentVersionLlmModel"
   has_many :enabled_llm_models, through: :enabled_agent_version_llm_models, source: :llm_model
 
   validates :agent_id, presence: true
