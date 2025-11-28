153bb03f5cd3b42d7b79807f11ac94372054dd61
Vincent Pellé <vincent.pelle@pm.me>
2025-11-28 15:04:00 +0100

    set french as default language

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module SaumonDruid
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :fr
   end
 end
