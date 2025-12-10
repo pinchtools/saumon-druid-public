04ce8f9dad8fd86943158fcf4925a25e4d0d3c31
Vincent Pellé <vincent.pelle@pm.me>
2025-12-10 14:37:52 +0100

    fix linter warning

--- db/migrate/20251207151443_create_an_searches.rb
+++ db/migrate/20251207151443_create_an_searches.rb
@@ -12,7 +12,7 @@ class CreateAnSearches < ActiveRecord::Migration[8.1]
 
     add_index :an_searches, :fts, using: :gin
     add_index :an_searches, :trigram, using: :gin, opclass: :gin_trgm_ops
-    add_index :an_searches, [:searchable_type, :searchable_id], unique: true
+    add_index :an_searches, [ :searchable_type, :searchable_id ], unique: true
   end
 
   def down
