5ccb45351c4848e0350643e01ea8ea046b9fc257
Vincent Pellé <vincent.pelle@pm.me>
2025-12-12 11:23:25 +0100

    fix linter warning

--- db/migrate/20251212100104_add_date_indexes_on_an_terms.rb
+++ db/migrate/20251212100104_add_date_indexes_on_an_terms.rb
@@ -1,14 +1,14 @@
 class AddDateIndexesOnAnTerms < ActiveRecord::Migration[8.1]
   def change
-    add_index :an_terms, [:start_date, :end_date], name: "index_terms_on_dates"
+    add_index :an_terms, [ :start_date, :end_date ], name: "index_terms_on_dates"
 
     add_index :an_terms,
-              [:an_stakeholder_id, :end_date, :start_date],
+              [ :an_stakeholder_id, :end_date, :start_date ],
               name: "index_an_terms_by_stakeholder_active",
               where: "start_date IS NOT NULL AND end_date IS NULL"
 
     add_index :an_terms,
-              [:an_stakeholder_id, :start_date, :end_date],
+              [ :an_stakeholder_id, :start_date, :end_date ],
               name: 'index_an_terms_by_stakeholder_past',
               where: 'start_date IS NOT NULL AND end_date IS NOT NULL'
   end
