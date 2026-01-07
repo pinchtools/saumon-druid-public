aedbaee3d63b928d5b3fff3be7efcfaa227c505c
Vincent Pellé <vincent.pelle@pm.me>
2026-01-07 10:24:31 +0100

    mv trace_id, message_id attr to base_input class

--- app/models/agent/input/base_input.rb
+++ app/models/agent/input/base_input.rb
@@ -3,6 +3,9 @@ class Agent::Input::BaseInput
   include ActiveModel::Validations
   include ActiveModel::Attributes
 
+  attribute :trace_id, :string
+  attribute :message_id, :string
+
   validates :trace_id, presence: true
   validates :message_id, presence: true
 end
--- app/models/agent/input/query_planner_input.rb
+++ app/models/agent/input/query_planner_input.rb
@@ -1,7 +1,5 @@
 class Agent::Input::QueryPlannerInput < Agent::Input::BaseInput
   attribute :question, :string
-  attribute :trace_id, :string
-  attribute :message_id, :string
 
   validates :question, presence: true, length: { minimum: 5, maximum: 500 }
 end
