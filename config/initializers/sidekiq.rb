f79664763f70826adccf8a968055b97a71b0b669
Vincent Pellé <vincent.pelle@pm.me>
2026-01-10 15:07:17 +0100

    persit Current object through sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require "sidekiq/middleware/current_attributes"
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch("REDIS_SIDEKIQ_URL", "redis://redis:6379/1") }
 end
@@ -5,3 +7,6 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch("REDIS_SIDEKIQ_URL", "redis://redis:6379/1") }
 end
+
+# Persist CurrentAttributes from web request to Sidekiq job
+Sidekiq::CurrentAttributes.persist("Current")
